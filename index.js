bd08cd18f66a0a7b247bb44826f88c54d559ff6c
Liam Tinney <ltinney@powerhrg.com>
2020-07-20 13:54:18 -0400

    Done.

--- index.js
+++ index.js
@@ -7,32 +7,24 @@ class Formatter {
     return string.replace( /[^A-Za-z0-9 '-]/g, '' )
   }
 
-  static titleize(string){
-    const excludes = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+  static titleize(str){
+    const exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
     let concludes = []
 
-    let words = string.split(' ')
+    let words = str.split(' ')
 
     for(let jawn = 0; jawn < words.length; jawn++){
       if(jawn ==0){
         concludes.push(this.capitalize(words[jawn]))
       } else {
-        if(excludes.includes(words[jawn)){
+        if(exceptions.includes(words[jawn])){
           concludes.push(words[jawn])
-        } else {
+        } else{
           concludes.push(this.capitalize(words[jawn]))
         }
       }
     }
     return concludes.join(' ')
   }
-
-}
-
-
-
-
-
-
 
 }
