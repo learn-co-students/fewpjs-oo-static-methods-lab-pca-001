abb50d29d4ac277d8b9fe6b3ee7ca1fbf8603055
Andrew Kloecker <lt.kvothe@gmail.com>
2020-07-20 10:07:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.length <= 1 ? str.toUpperCase() : str
+      .split(' ')
+      .map( x => x[0].toUpperCase() + x.slice(1))
+      .join(' ');
+  }
+
+  static sanitize(str){
+    if(str.length <= 1 ){
+      return str.match(/[a-zA-Z0-9\s]/) ? str : '';
+    }
+    return str.split(' ')
+    .map( x=> x.split('').filter( y => y.match(/[a-zA-Z0-9\'\-]/)).join('') )
+    .join(' ');
+  }
+
+  static titleize(str){
+
+    if(str.length <= 1 ){
+      return str.toUpperCase();
+    } else {
+      str = str[0].toUpperCase() + str.slice(1);
+      const arr = str.split(' ');
+      return arr.map( word => {
+        if(word.length > 2 && (word !== 'the' & word !== 'for' && word !== 'and' &&word !== 'but' && word !== 'from')){
+          return this.capitalize(word);
+        }else {
+          return word;
+        }
+      })
+      .join(' ');
+    }
+
+  }
+
 }
